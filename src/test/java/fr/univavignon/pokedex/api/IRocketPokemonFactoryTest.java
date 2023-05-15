4fc52c65b398c0e7d0aa287d217aa0095882258a
hicham-anbari <hicham.anb84@gmail.com>
2023-05-15 23:25:55 +0200

    fix: 100% coverage TP6

--- src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
@@ -31,5 +31,17 @@ public class IRocketPokemonFactoryTest {
         assertNotEquals(bulbizarre.getStamina(), bulbizarreRocket.getStamina());
         // Verifier que les IV est égale à 0 ou 1
         assertTrue(bulbizarreRocket.getIv() == 0 || bulbizarreRocket.getIv() == 1);
+
+        Pokemon pikachu = rocketPokemonFactory.createPokemon(-1, 0, 0, 0, 0);
+        assertEquals(pikachu.getIndex(), -1);
+        assertEquals(pikachu.getName(), "Ash's Pikachu");
+        assertEquals(pikachu.getAttack(), 1000);
+        assertEquals(pikachu.getDefense(), 1000);
+        assertEquals(pikachu.getStamina(), 1000);
+        assertEquals(pikachu.getCp(), 0);
+        assertEquals(pikachu.getHp(), 0);
+        assertEquals(pikachu.getDust(), 0);
+        assertEquals(pikachu.getCandy(), 0);
+        assertEquals(pikachu.getIv(), 0, 0);
     }
 }
